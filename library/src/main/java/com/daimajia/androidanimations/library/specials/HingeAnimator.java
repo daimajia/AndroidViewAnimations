d644cb54d56e2163d70038014d279c38c6ef0721
daimajia <daimajia@gmail.com>
2014-06-24 09:43:00 +0800

    Licence description

--- library/src/main/java/com/daimajia/androidanimations/library/specials/HingeAnimator.java
+++ library/src/main/java/com/daimajia/androidanimations/library/specials/HingeAnimator.java
@@ -1,15 +1,34 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 daimajia
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.daimajia.androidanimations.library.specials;
 
-import android.util.Log;
 import android.view.View;
-import android.view.animation.Interpolator;
 
 import com.daimajia.androidanimations.library.BaseViewAnimator;
 import com.nineoldandroids.animation.ObjectAnimator;
 
-/**
- * Created by daimajia on 14-6-21.
- */
 public class HingeAnimator extends BaseViewAnimator{
     @Override
     protected void prepare(View target) {
@@ -23,22 +42,6 @@ public class HingeAnimator extends BaseViewAnimator{
                 ObjectAnimator.ofFloat(target,"pivotY",y,y,y,y,y,y)
         );
         setDuration(1300);
-//        setInterpolator(new HesitateInterpolator(1300,690000,0));
-    }
-    public class HesitateInterpolator implements Interpolator {
-        private float mDuration;
-        private float mChange;
-        private float mBegin;
-        public HesitateInterpolator(float d,float c, float b) {
-            mBegin = b;
-            mDuration = d;
-            mChange = c;
-        }
-        public float getInterpolation(float t) {
-            float v = (float)(-mChange/2 * (Math.cos(Math.PI*t/mDuration) - 1) + mBegin);
-            Log.e("Value",v+ "");
-            return v;
-        }
     }
 }
 
